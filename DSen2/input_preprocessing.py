8f7b8de41779799cecb10850e69608bb968a984f
matciotola <matciotola@gmail.com>
2023-06-16 15:04:23 +0200

    minor bug fix

--- DSen2/input_preprocessing.py
+++ DSen2/input_preprocessing.py
@@ -3,7 +3,7 @@ import torch
 from torchvision.transforms import InterpolationMode
 from torchvision.transforms.functional import resize, gaussian_blur, pad
 from torch.nn.functional import avg_pool2d
-
+from typing import Tuple
 
 def normalize(img, scale=2000):
     normalized = img / scale
@@ -50,44 +50,47 @@ def upsample_protocol(img, shape):
     return img_hr
 
 
-def get_test_patches_20(dset_10, dset_20, patchSize=128, border=4, interp=True):
-    PATCH_SIZE_HR = (patchSize, patchSize)
-    PATCH_SIZE_LR = [p // 2 for p in PATCH_SIZE_HR]
-    BORDER_HR = border
-    BORDER_LR = BORDER_HR // 2
+def get_test_patches_20(dset_10, dset_20, patch_size=128, border=4, interp=True):
+    patch_size_hr = (patch_size, patch_size)
+    patch_size_lr = [p // 2 for p in patch_size_hr]
+    border_hr = border
+    border_lr = border_hr // 2
 
     # Mirror the data at the borders to have the same dimensions as the input
-    dset_10 = pad(dset_10, [BORDER_HR, BORDER_HR, BORDER_HR, BORDER_HR], padding_mode='symmetric')
-    dset_20 = pad(dset_20, [BORDER_LR, BORDER_LR, BORDER_LR, BORDER_LR], padding_mode='symmetric')
+    # dset_10 = pad(dset_10, (border_hr, border_hr, border_hr, border_hr), padding_mode='symmetric')
+    # dset_20 = pad(dset_20, [border_lr, border_lr, border_lr, border_lr], padding_mode='symmetric')
 
-    BANDS10 = dset_10.shape[1]
-    BANDS20 = dset_20.shape[1]
-    patchesAlongi = (dset_20.shape[2] - 2 * BORDER_LR) // (PATCH_SIZE_LR[0] - 2 * BORDER_LR)
-    patchesAlongj = (dset_20.shape[3] - 2 * BORDER_LR) // (PATCH_SIZE_LR[1] - 2 * BORDER_LR)
+    dset_10 = symm_pad(dset_10, (border_hr, border_hr, border_hr, border_hr))
+    dset_20 = symm_pad(dset_20, (border_lr, border_lr, border_lr, border_lr))
 
-    nr_patches = (patchesAlongi + 1) * (patchesAlongj + 1)
+    bands10 = dset_10.shape[1]
+    bands20 = dset_20.shape[1]
+    patches_along_i = (dset_20.shape[2] - 2 * border_lr) // (patch_size_lr[0] - 2 * border_lr)
+    patches_along_j = (dset_20.shape[3] - 2 * border_lr) // (patch_size_lr[1] - 2 * border_lr)
 
-    image_20 = torch.zeros(((nr_patches, BANDS20) + tuple(PATCH_SIZE_LR)), dtype=torch.float32)
-    image_10 = torch.zeros(((nr_patches, BANDS10) + PATCH_SIZE_HR), dtype=torch.float32)
+    nr_patches = (patches_along_i + 1) * (patches_along_j + 1)
 
-    range_i = np.arange(0, (dset_20.shape[2] - 2 * BORDER_LR) // (PATCH_SIZE_LR[0] - 2 * BORDER_LR)) * (
-            PATCH_SIZE_LR[0] - 2 * BORDER_LR)
-    range_j = np.arange(0, (dset_20.shape[3] - 2 * BORDER_LR) // (PATCH_SIZE_LR[1] - 2 * BORDER_LR)) * (
-            PATCH_SIZE_LR[1] - 2 * BORDER_LR)
+    image_20 = torch.zeros(((nr_patches, bands20) + tuple(patch_size_lr)), dtype=torch.float32)
+    image_10 = torch.zeros(((nr_patches, bands10) + patch_size_hr), dtype=torch.float32)
 
-    if not ((dset_20.shape[0] - 2 * BORDER_LR) % (PATCH_SIZE_LR[0] - 2 * BORDER_LR)) == 0:
-        range_i = np.append(range_i, (dset_20.shape[2] - PATCH_SIZE_LR[0]))
-    if not ((dset_20.shape[1] - 2 * BORDER_LR) % (PATCH_SIZE_LR[1] - 2 * BORDER_LR)) == 0:
-        range_j = np.append(range_j, (dset_20.shape[3] - PATCH_SIZE_LR[1]))
+    range_i = np.arange(0, (dset_20.shape[2] - 2 * border_lr) // (patch_size_lr[0] - 2 * border_lr)) * (
+            patch_size_lr[0] - 2 * border_lr)
+    range_j = np.arange(0, (dset_20.shape[3] - 2 * border_lr) // (patch_size_lr[1] - 2 * border_lr)) * (
+            patch_size_lr[1] - 2 * border_lr)
 
-    pCount = 0
+    if not ((dset_20.shape[0] - 2 * border_lr) % (patch_size_lr[0] - 2 * border_lr)) == 0:
+        range_i = np.append(range_i, (dset_20.shape[2] - patch_size_lr[0]))
+    if not ((dset_20.shape[1] - 2 * border_lr) % (patch_size_lr[1] - 2 * border_lr)) == 0:
+        range_j = np.append(range_j, (dset_20.shape[3] - patch_size_lr[1]))
+
+    p_count = 0
     for ii in range_i.astype(int):
         for jj in range_j.astype(int):
-            crop_point_lr = [ii, jj, ii + PATCH_SIZE_LR[0], jj + PATCH_SIZE_LR[1]]
+            crop_point_lr = [ii, jj, ii + patch_size_lr[0], jj + patch_size_lr[1]]
             crop_point_hr = [p * 2 for p in crop_point_lr]
-            image_20[pCount] = dset_20[:, :, crop_point_lr[0]:crop_point_lr[2], crop_point_lr[1]:crop_point_lr[3]]
-            image_10[pCount] = dset_10[:, :, crop_point_hr[0]:crop_point_hr[2], crop_point_hr[1]:crop_point_hr[3]]
-            pCount += 1
+            image_20[p_count] = dset_20[:, :, crop_point_lr[0]:crop_point_lr[2], crop_point_lr[1]:crop_point_lr[3]]
+            image_10[p_count] = dset_10[:, :, crop_point_hr[0]:crop_point_hr[2], crop_point_hr[1]:crop_point_hr[3]]
+            p_count += 1
 
     image_10_shape = image_10.shape
 
@@ -99,51 +102,55 @@ def get_test_patches_20(dset_10, dset_20, patchSize=128, border=4, interp=True):
     return image_10, data20_interp
 
 
-def get_test_patches_60(dset_10, dset_20, dset_60, patchSize=128, border=8, interp=True):
-    PATCH_SIZE_10 = (patchSize, patchSize)
-    PATCH_SIZE_20 = [p // 2 for p in PATCH_SIZE_10]
-    PATCH_SIZE_60 = [p // 6 for p in PATCH_SIZE_10]
-    BORDER_10 = border
-    BORDER_20 = BORDER_10 // 2
-    BORDER_60 = BORDER_10 // 6
+def get_test_patches_60(dset_10, dset_20, dset_60, patch_size=128, border=8, interp=True):
+    patch_size_10 = (patch_size, patch_size)
+    patch_size_20 = [p // 2 for p in patch_size_10]
+    patch_size_60 = [p // 6 for p in patch_size_10]
+    border_10 = border
+    border_20 = border_10 // 2
+    border_60 = border_10 // 6
 
     # Mirror the data at the borders to have the same dimensions as the input
-    dset_10 = pad(dset_10, [BORDER_10, BORDER_10, BORDER_10, BORDER_10], padding_mode='symmetric')
-    dset_20 = pad(dset_20, [BORDER_20, BORDER_20, BORDER_20, BORDER_20], padding_mode='symmetric')
-    dset_60 = pad(dset_60, [BORDER_60, BORDER_60, BORDER_60, BORDER_60], padding_mode='symmetric')
+    # dset_10 = pad(dset_10, [border_10, border_10, border_10, border_10], padding_mode='symmetric')
+    # dset_20 = pad(dset_20, [border_20, border_20, border_20, border_20], padding_mode='symmetric')
+    # dset_60 = pad(dset_60, [border_60, border_60, border_60, border_60], padding_mode='symmetric')
 
-    BANDS10 = dset_10.shape[1]
-    BANDS20 = dset_20.shape[1]
-    BANDS60 = dset_60.shape[1]
-    patchesAlongi = (dset_60.shape[2] - 2 * BORDER_60) // (PATCH_SIZE_60[0] - 2 * BORDER_60)
-    patchesAlongj = (dset_60.shape[3] - 2 * BORDER_60) // (PATCH_SIZE_60[1] - 2 * BORDER_60)
+    dset_10 = symm_pad(dset_10, (border_10, border_10, border_10, border_10))
+    dset_20 = symm_pad(dset_20, (border_20, border_20, border_20, border_20))
+    dset_60 = symm_pad(dset_60, (border_60, border_60, border_60, border_60))
 
-    nr_patches = (patchesAlongi + 1) * (patchesAlongj + 1)
+    bands10 = dset_10.shape[1]
+    bands20 = dset_20.shape[1]
+    bands60 = dset_60.shape[1]
+    patches_along_i = (dset_60.shape[2] - 2 * border_60) // (patch_size_60[0] - 2 * border_60)
+    patches_along_j = (dset_60.shape[3] - 2 * border_60) // (patch_size_60[1] - 2 * border_60)
 
-    image_10 = torch.zeros((nr_patches, BANDS10) + PATCH_SIZE_10, dtype=torch.float32)
-    image_20 = torch.zeros((nr_patches, BANDS20) + tuple(PATCH_SIZE_20), dtype=torch.float32)
-    image_60 = torch.zeros((nr_patches, BANDS60) + tuple(PATCH_SIZE_60), dtype=torch.float32)
+    nr_patches = (patches_along_i + 1) * (patches_along_j + 1)
 
-    range_i = np.arange(0, (dset_60.shape[2] - 2 * BORDER_60) // (PATCH_SIZE_60[0] - 2 * BORDER_60)) * (
-            PATCH_SIZE_60[0] - 2 * BORDER_60)
-    range_j = np.arange(0, (dset_60.shape[3] - 2 * BORDER_60) // (PATCH_SIZE_60[1] - 2 * BORDER_60)) * (
-            PATCH_SIZE_60[1] - 2 * BORDER_60)
+    image_10 = torch.zeros((nr_patches, bands10) + patch_size_10, dtype=torch.float32)
+    image_20 = torch.zeros((nr_patches, bands20) + tuple(patch_size_20), dtype=torch.float32)
+    image_60 = torch.zeros((nr_patches, bands60) + tuple(patch_size_60), dtype=torch.float32)
 
-    if not ((dset_60.shape[0] - 2 * BORDER_60 % PATCH_SIZE_60[0] - 2 * BORDER_60) == 0):
-        range_i = np.append(range_i, (dset_60.shape[2] - PATCH_SIZE_60[0]))
-    if not ((dset_60.shape[1] - 2 * BORDER_60 % PATCH_SIZE_60[1] - 2 * BORDER_60) == 0):
-        range_j = np.append(range_j, (dset_60.shape[3] - PATCH_SIZE_60[1]))
+    range_i = np.arange(0, (dset_60.shape[2] - 2 * border_60) // (patch_size_60[0] - 2 * border_60)) * (
+            patch_size_60[0] - 2 * border_60)
+    range_j = np.arange(0, (dset_60.shape[3] - 2 * border_60) // (patch_size_60[1] - 2 * border_60)) * (
+            patch_size_60[1] - 2 * border_60)
 
-    pCount = 0
+    if not ((dset_60.shape[0] - 2 * border_60 % patch_size_60[0] - 2 * border_60) == 0):
+        range_i = np.append(range_i, (dset_60.shape[2] - patch_size_60[0]))
+    if not ((dset_60.shape[1] - 2 * border_60 % patch_size_60[1] - 2 * border_60) == 0):
+        range_j = np.append(range_j, (dset_60.shape[3] - patch_size_60[1]))
+
+    p_count = 0
     for ii in range_i.astype(int):
         for jj in range_j.astype(int):
-            crop_point_60 = [ii, jj, ii + PATCH_SIZE_60[0], jj + PATCH_SIZE_60[1]]
+            crop_point_60 = [ii, jj, ii + patch_size_60[0], jj + patch_size_60[1]]
             crop_point_10 = [p * 6 for p in crop_point_60]
             crop_point_20 = [p * 3 for p in crop_point_60]
-            image_10[pCount] = dset_10[:, :, crop_point_10[0]:crop_point_10[2], crop_point_10[1]:crop_point_10[3]]
-            image_20[pCount] = dset_20[:, :, crop_point_20[0]:crop_point_20[2], crop_point_20[1]:crop_point_20[3]]
-            image_60[pCount] = dset_60[:, :, crop_point_60[0]:crop_point_60[2], crop_point_60[1]:crop_point_60[3]]
-            pCount += 1
+            image_10[p_count] = dset_10[:, :, crop_point_10[0]:crop_point_10[2], crop_point_10[1]:crop_point_10[3]]
+            image_20[p_count] = dset_20[:, :, crop_point_20[0]:crop_point_20[2], crop_point_20[1]:crop_point_20[3]]
+            image_60[p_count] = dset_60[:, :, crop_point_60[0]:crop_point_60[2], crop_point_60[1]:crop_point_60[3]]
+            p_count += 1
 
     image_10_shape = image_10.shape
 
@@ -186,8 +193,31 @@ def recompose_images(a, border, size=None):
                 if xpoint > size[2] - patch_size:
                     xpoint = size[2] - patch_size
                 images[:, :, ypoint:ypoint + patch_size, xpoint:xpoint + patch_size] = a[current_patch, :,
-                                                                                       border:a.shape[2] - border,
-                                                                                       border:a.shape[3] - border]
+                                                                                         border:a.shape[2] - border,
+                                                                                         border:a.shape[3] - border]
                 current_patch += 1
 
     return images
+
+
+def symm_pad(im: torch.Tensor, padding: Tuple[int, int, int, int]):
+    h, w = im.shape[-2:]
+    left, right, top, bottom = padding
+
+    x_idx = np.arange(-left, w + right)
+    y_idx = np.arange(-top, h + bottom)
+
+    def reflect(x, minx, maxx):
+        """ Reflects an array around two points making a triangular waveform that ramps up
+        and down,  allowing for pad lengths greater than the input length """
+        rng = maxx - minx
+        double_rng = 2 * rng
+        mod = np.fmod(x - minx, double_rng)
+        normed_mod = np.where(mod < 0, mod + double_rng, mod)
+        out = np.where(normed_mod >= rng, double_rng - normed_mod, normed_mod) + minx
+        return np.array(out, dtype=x.dtype)
+
+    x_pad = reflect(x_idx, -0.5, w - 0.5)
+    y_pad = reflect(y_idx, -0.5, h - 0.5)
+    xx, yy = np.meshgrid(x_pad, y_pad)
+    return im[..., yy, xx]
