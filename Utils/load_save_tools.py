92c293996a343919124b28c9e09ce10b9432da0b
matciotola <matciotola@gmail.com>
2024-08-26 10:17:31 +0200

    change paradigm from TIFF to MAT

--- Utils/load_save_tools.py
+++ Utils/load_save_tools.py
@@ -1,67 +1,39 @@
+import os.path
+
 import torch
-from osgeo import gdal
-import os
+from scipy import io
 import numpy as np
+from Utils.imresize_bicubic import imresize
+# from Utils.interpolator_tools import interp23tap
+
+def open_mat(path):
+    # Open .mat file
+    dic_file = io.loadmat(path)
+
+    # Extract fields and convert them in float32 numpy arrays
+    bands_10_np = dic_file['S2_10'].astype(np.float64)
+    bands_20_np = dic_file['S2_20'].astype(np.float64)
+    bands_60_np = dic_file['S2_60'].astype(np.float64)
+
+    if 'S2_GT' in dic_file.keys():
+        gt_np = dic_file['S2_GT'].astype(np.float64)
+        gt = torch.from_numpy(np.moveaxis(gt_np, -1, 0)[None, :, :, :])
+    else:
+        gt = None
+
+    # Convert numpy arrays to torch tensors
+    bands_10 = torch.from_numpy(np.moveaxis(bands_10_np, -1, 0)[None, :, :, :])
+    bands_20 = torch.from_numpy(np.moveaxis(bands_20_np, -1, 0)[None, :, :, :])
+    bands_60 = torch.from_numpy(np.moveaxis(bands_60_np, -1, 0)[None, :, :, :])
+
+    return bands_10, bands_20, bands_60, gt
 
 
-def extract_info(filename):
-    ds = gdal.Open(filename)
-    geotransf = ds.GetGeoTransform()
-    proj = ds.GetProjection()
-    rows = ds.RasterYSize
-    cols = ds.RasterXSize
-    tp = ds.GetRasterBand(1).DataType
-    info = {'geo': geotransf, 'proj': proj, 'rows': rows, 'cols': cols, 'type': tp}
-    del ds
-    return info
-
-
-def save_tiff(img, root, filename, info, ratio=2, type='uint16', save_geo_info=True):
-
-    if type == 'uint16':
-        t = gdal.GDT_UInt16
-        np.round(np.clip(img, 0, 65535, out=img)).astype(np.uint16)
-    elif type == 'float32':
-        t = gdal.GDT_Float32
-
-    if not os.path.exists(root):
-        os.makedirs(root)
-    r = ratio
-    rows = info['rows']//r
-    if not img.shape[0]==rows:
-        img = np.moveaxis(img,0,-1)
-    driver = gdal.GetDriverByName("GTiff")
-    driver.Register()
-    b = img
-
-    assert b.shape[0]==info['rows'] and b.shape[1]==info['cols'], \
-        "The dimensions of the image and the reference are not the same."
-    writer = driver.Create(os.path.join(root, filename), xsize=info['cols'],
-                           ysize=info['rows'], bands=b.shape[-1],
-                           eType=t)
-    os.chmod(os.path.join(root, filename), 0o0777)
-    for bb in range(b.shape[-1]):
-        hband = writer.GetRasterBand(bb + 1)
-        h = b[:, :, bb]
-        h = h.astype(type)
-        hband.WriteArray(h)
-        # hband.SetNoDataValue(np.nan)
-        # hband.FlushCache()
-    geo = info['geo']
-    proj = info['proj']
-    geo2 = [0]*len(geo)
-
-    if save_geo_info:
-        writer.SetGeoTransform(geo)
-        writer.SetProjection(proj)
-
-    del driver
-    del writer
+def save_mat(image, path, key):
+    if os.path.exists(path):
+        dic = io.loadmat(path)
+    else:
+        dic = {}
+    dic[key] = image
+    io.savemat(path, dic)
     return
-
-
-def open_tiff(path):
-    bands = gdal.Open(path)
-    bands = bands.ReadAsArray().astype('float32')
-    bands = torch.Tensor(bands)[None, :, :, :]
-    return bands
