25eafb3321aaa4af857cc5a77beae383608c748e
matciotola <matciotola@gmail.com>
2024-08-26 10:12:54 +0200

    re-implement evaluation protocols

--- Metrics/evaluation.py
+++ Metrics/evaluation.py
@@ -1,77 +1,55 @@
+import torch
 import torch.nn.functional as F
 from Utils.spectral_tools import gen_mtf, mtf_kernel_to_torch
-try:
-    import metrics_rr as rr
-    import metrics as fr
-    from coregistration import stacked_fineshift
-except:
-    from Metrics import metrics_rr as rr
-    from Metrics import metrics as fr
-    from Metrics.coregistration import stacked_fineshift
 
-def evaluation_rr(out_lr, bands_low_lr, ratio):
-    if out_lr.shape[1] == 2:
-        bands_low_lr = bands_low_lr[:, :-1, :, :]
+from . import metrics_rr as rr
+from . import metrics_fr as fr
+from .coregistration import fineshift
 
-    ergas = rr.ERGAS(ratio).to(out_lr.device)
-    sam = rr.SAM().to(out_lr.device)
-    q = rr.Q(out_lr.shape[1]).to(out_lr.device)
-    q2n = rr.Q2n().to(out_lr.device)
+def evaluation_rr_20(fused, gt, ratio, flag_cut=True, dim_cut=11, L=16):
+    if flag_cut:
+        fused = fused[:, :, dim_cut - 1:-dim_cut, dim_cut - 1:-dim_cut]
+        gt = gt[:, :, dim_cut - 1:-dim_cut, dim_cut - 1:-dim_cut]
 
-    ergas_index, _ = ergas(out_lr, bands_low_lr)
-    sam_index, _ = sam(out_lr, bands_low_lr)
-    q_index = q(out_lr, bands_low_lr)
-    q2n_index, _ = q2n(out_lr, bands_low_lr)
+    fused = torch.clip(fused, 0, 2 ** L)
 
-    return ergas_index.item(), sam_index.item(), q_index.item(), q2n_index.item()
+    ergas = rr.ERGAS(ratio).to(fused.device)
+    sam = rr.SAM().to(fused.device)
+    q2n = rr.Q2n().to(fused.device)
+
+    ergas_index, _ = ergas(fused, gt)
+    sam_index, _ = sam(fused, gt)
+    q2n_index, _ = q2n(fused, gt)
+
+    return ergas_index.item(), sam_index.item(), q2n_index.item()
 
 
-def evaluation_fr(out, bands_high, bands_low_lr, ratio):
+def evaluation_fr_20(fused, bands_10, bands_20, ratio, sensor):
+    starting = 1
+    sigma = 4
 
-    if ratio == 2:
-        sensor = 'S2-20'
-        starting = 1
-        sigma = 4
-    elif ratio == 6 and out.shape[1] == 3:
-        sensor = 'S2-60'
-        starting = 3
-        sigma = 6
-    else:
-        sensor = 'S2-60_bis'
-        starting = 3
-        sigma = 6
-        bands_low_lr = bands_low_lr[:, :-1, :, :]
+    kernel = mtf_kernel_to_torch(gen_mtf(ratio, sensor, nbands=fused.shape[1]))
 
-    kernel = mtf_kernel_to_torch(gen_mtf(ratio, sensor))
+    filter = torch.nn.Conv2d(fused.shape[1], fused.shape[1], kernel_size=kernel.shape[2], groups=fused.shape[1],
+                             padding='same', padding_mode='replicate', bias=False)
+    filter.weight = torch.nn.Parameter(kernel.type(fused.dtype).to(fused.device))
+    filter.weight.requires_grad = False
 
-    out_lp = F.conv2d(out, kernel.type(out.dtype).to(out.device), padding='same', groups=out.shape[1])
+    fused_lp = filter(fused)
+    fused_lp = fineshift(fused_lp, 1, 1, fused_lp.device)
+    fused_lr = fused_lp[:, :, starting::ratio, starting::ratio]
+    ergas = rr.ERGAS(ratio).to(fused.device)
+    sam = rr.SAM().to(fused.device)
+    q2n = rr.Q2n().to(fused.device)
+    d_rho = fr.D_rho(sigma).to(fused.device)
 
-    out_lp_shifted = stacked_fineshift(out_lp, 1, 1)
-    out_lr = out_lp_shifted[:, :, starting::ratio, starting::ratio]
+    # Spectral Assessment
+    ergas_index, _ = ergas(fused_lr, bands_20)
+    sam_index, _ = sam(fused_lr, bands_20)
+    q2n_index, _ = q2n(fused_lr, bands_20)
+    d_lambda_index = 1 - q2n_index
 
-    ergas = rr.ERGAS(ratio).to(out.device)
-    sam = rr.SAM().to(out.device)
-    q = rr.Q(out.shape[1]).to(out.device)
-    q2n = rr.Q2n().to(out.device)
-    d_rho = fr.D_rho(sigma).to(out.device)
+    # Spatial Assessment
+    d_rho_index, _ = d_rho(fused, bands_10)
 
-    ergas_index, _ = ergas(out_lr, bands_low_lr)
-    sam_index, _ = sam(out_lr, bands_low_lr)
-    q_index = q(out_lr, bands_low_lr)
-    q2n_index, _ = q2n(out_lr, bands_low_lr)
-
-    d_rho_index, _ = d_rho(out, bands_high)
-
-    return ergas_index.item(), sam_index.item(), q_index.item(), q2n_index.item(), d_rho_index.item()
-
-
-if __name__ == '__main__':
-    from Utils.load_save_tools import open_tiff
-
-    bands_10 = open_tiff('/media/matteo/T7/Dataset_Ugliano/10/New_York.tif')
-    bands_20 = open_tiff('/media/matteo/T7/Dataset_Ugliano/20/New_York.tif')
-    out = open_tiff('/media/matteo/T7/outputs_Ugliano/New_York/FR/20/SYNTH-BDSD.tiff')
-
-    ciccio = evaluation_fr(out, bands_10, bands_20, 2)
-
-
+    return (d_lambda_index.item(), ergas_index.item(), sam_index.item(), d_rho_index.item())
