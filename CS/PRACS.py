0eaae71a5e3b94a3bb666bca2fea102c50fe4e0a
matciotola <matciotola@gmail.com>
2024-08-26 10:15:14 +0200

    change nomenclature

--- CS/PRACS.py
+++ CS/PRACS.py
@@ -5,29 +5,24 @@ from Utils.imresize_bicubic import imresize
 
 
 def PRACS(ordered_dict, beta=0.95):
-    bands_low = torch.clone(ordered_dict.bands_low)
-    bands_high = torch.clone(ordered_dict.bands_high)
+    ms = torch.clone(ordered_dict.ms)
+    pan = torch.clone(ordered_dict.pan)
     ratio = ordered_dict.ratio
 
-    B, C, H, W = bands_low.shape
+    B, C, H, W = ms.shape
 
-    bands_low_hm = (bands_low - torch.mean(bands_low, dim=(2, 3), keepdim=True) + torch.mean(bands_high, dim=(2, 3),
+    ms_hm = (ms - torch.mean(ms, dim=(2, 3), keepdim=True) + torch.mean(pan, dim=(2, 3),
                                                                                              keepdim=True) / torch.std(
-        bands_high, dim=(2, 3), keepdim=True) * torch.std(bands_low, dim=(2, 3), keepdim=True)) * torch.std(bands_high,
-                                                                                                            dim=(2, 3),
-                                                                                                            keepdim=True) / torch.std(
-        bands_low, dim=(2, 3), keepdim=True)
-    bands_low_hm = torch.clip(bands_low_hm, 0, bands_low_hm.max())
+        pan, dim=(2, 3), keepdim=True) * torch.std(ms, dim=(2, 3), keepdim=True)) * torch.std(pan, dim=(2, 3), keepdim=True) / torch.std(ms, dim=(2, 3), keepdim=True)
+    ms_hm = torch.clip(ms_hm, 0, ms_hm.max())
 
-    bands_high_lp = resize(
-        resize(bands_high, [bands_high.shape[2] // ratio, bands_high.shape[3] // ratio], interpolation=Inter.BICUBIC,
-               antialias=True), [bands_high.shape[2], bands_high.shape[3]], interpolation=Inter.BICUBIC, antialias=True)
+    pan_lp = imresize(imresize(pan, scale=1 / ratio), scale=ratio)
 
-    bb = torch.cat([torch.ones((B, 1, H, W), dtype=bands_low_hm.dtype, device=bands_low_hm.device), bands_low_hm],
+    bb = torch.cat([torch.ones((B, 1, H, W), dtype=ms_hm.dtype, device=ms_hm.device), ms_hm],
                    dim=1)
     bb = torch.flatten(bb, start_dim=2).transpose(1, 2)
-    bands_high_lp_f = torch.flatten(bands_high_lp, start_dim=2).transpose(1, 2)
-    alpha = regress(bands_high_lp_f, bb)
+    pan_lp_f = torch.flatten(pan_lp, start_dim=2).transpose(1, 2)
+    alpha = regress(pan_lp_f, bb)
 
     aux = torch.matmul(bb, alpha)
 
@@ -37,18 +32,21 @@ def PRACS(ordered_dict, beta=0.95):
     for b in range(B):
         corr_bands = []
         for c in range(C):
-            stack = torch.flatten(torch.cat([img[b, :, :, :], bands_low_hm[b, c, None, :, :]], dim=0), start_dim=1)
+            stack = torch.flatten(torch.cat([img[b, :, :, :], ms_hm[b, c, None, :, :]], dim=0), start_dim=1)
             corr_bands.append(torch.corrcoef(stack)[0, 1])
         corr_bands = torch.vstack(corr_bands)
         corr_coeffs.append(corr_bands[None, :, :])
 
     corr_coeffs = torch.vstack(corr_coeffs)[:, :, :, None]
 
-    img_h = corr_coeffs * bands_high.repeat(1, C, 1, 1) + (1 - corr_coeffs) * bands_low_hm
+    img_h = corr_coeffs * pan.repeat(1, C, 1, 1) + (1 - corr_coeffs) * ms_hm
 
-    img_h_lp = resize(
-        resize(img_h, [bands_high.shape[2] // ratio, bands_high.shape[3] // ratio], interpolation=Inter.BICUBIC,
-               antialias=True), [bands_high.shape[2], bands_high.shape[3]], interpolation=Inter.BICUBIC, antialias=True)
+    # img_h_lp = resize(
+    #     resize(img_h, [pan.shape[2] // ratio, pan.shape[3] // ratio], interpolation=Inter.BICUBIC,
+    #            antialias=True), [pan.shape[2], pan.shape[3]], interpolation=Inter.BICUBIC, antialias=True)
+
+    img_h_lp = imresize(imresize(img_h, scale=1 / ratio), scale=ratio)
+
     img_h_lp_f = torch.flatten(img_h_lp, start_dim=2)
     gamma = []
     for i in range(C):
@@ -66,28 +64,28 @@ def PRACS(ordered_dict, beta=0.95):
     delta = img_h - img_prime - (
                 torch.mean(img_h, dim=(2, 3), keepdim=True) - torch.mean(img_prime, dim=(2, 3), keepdim=True))
 
-    aux3 = torch.mean(torch.std(bands_low, dim=(2, 3)), dim=1)
+    aux3 = torch.mean(torch.std(ms, dim=(2, 3)), dim=1)
 
     w = []
     for b in range(B):
         w_bands = []
         for c in range(C):
-            stack = torch.flatten(torch.cat([img_prime[b, c, None, :, :], bands_low[b, c, None, :, :]], dim=0),
+            stack = torch.flatten(torch.cat([img_prime[b, c, None, :, :], ms[b, c, None, :, :]], dim=0),
                                   start_dim=1)
             w_bands.append(torch.corrcoef(stack)[0, 1])
         w_bands = torch.vstack(w_bands)
         w.append(w_bands[None, :, :, None])
     w = torch.vstack(w)
-    w = beta * w * torch.std(bands_low, dim=(2, 3), keepdim=True) / aux3
+    w = beta * w * torch.std(ms, dim=(2, 3), keepdim=True) / aux3
 
     L_i = []
     for b in range(B):
         L_i_bands = []
         for c in range(C):
-            stack = torch.flatten(torch.cat([img[b, 0, None, :, :], bands_low[b, c, None, :, :]], dim=0), start_dim=1)
+            stack = torch.flatten(torch.cat([img[b, 0, None, :, :], ms[b, c, None, :, :]], dim=0), start_dim=1)
 
             rho = torch.corrcoef(stack)[0, 1]
-            aux = 1 - abs(1 - rho * bands_low[b, c, None, :, :] / img_prime[b, c, None, :, :])
+            aux = 1 - abs(1 - rho * ms[b, c, None, :, :] / img_prime[b, c, None, :, :])
 
             L_i_bands.append(torch.reshape(aux, (1, 1, H, W)))
         L_i_bands = torch.cat(L_i_bands, 1)
@@ -95,7 +93,7 @@ def PRACS(ordered_dict, beta=0.95):
     L_i = torch.cat(L_i, 0)
 
     det = w * L_i * delta
-    fused = bands_low + det
+    fused = ms + det
 
     return fused
 
