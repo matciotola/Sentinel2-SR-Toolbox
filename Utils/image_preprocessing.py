daecede4df94e19f6eafda990a4a9b85f29ad07d
matciotola <matciotola@gmail.com>
2024-08-26 10:16:08 +0200

    change from TIFF to MAT implementation

--- Utils/image_preprocessing.py
+++ Utils/image_preprocessing.py
@@ -1,26 +1,15 @@
 from torch.nn import functional as F
 from Utils.spectral_tools import mtf
 
-def downsample_protocol(bands_high, bands_intermediate, bands_low, ratio):
+def downsample_protocol(bands_10, bands_20, bands_60, ratio):
 
-    if ratio == 2:
 
-        bands_high_lp = mtf(bands_high, 'S2_10', ratio)
-        bands_low_lp = mtf(bands_low, 'S2_20', ratio)
+    bands_10_lp = mtf(bands_10, 'S2_10', ratio)
+    bands_20_lp = mtf(bands_20, 'S2_20', ratio)
+    bands_60_lp = mtf(bands_60, 'S2_60', ratio)
 
-        bands_high_lr = F.interpolate(bands_high_lp, scale_factor= 1/ratio, mode='nearest-exact')
-        bands_low_lr = F.interpolate(bands_low_lp, scale_factor= 1/ratio, mode='nearest-exact')
+    bands_10_lr = F.interpolate(bands_10_lp, scale_factor=1/ratio, mode='nearest-exact')
+    bands_20_lr = F.interpolate(bands_20_lp, scale_factor=1/ratio, mode='nearest-exact')
+    bands_60_lr = F.interpolate(bands_60_lp, scale_factor=1/ratio, mode='nearest-exact')
 
-        bands_intermediate_lr = None
-
-    elif ratio == 6:
-
-        bands_high_lp = mtf(bands_high, 'S2_10', ratio)
-        bands_intermediate_lp = mtf(bands_intermediate, 'S2_20', ratio)
-        bands_low_lp = mtf(bands_low, 'S2_60', ratio)
-
-        bands_high_lr = F.interpolate(bands_high_lp, scale_factor= 1/ratio, mode='nearest-exact')
-        bands_intermediate_lr = F.interpolate(bands_intermediate_lp, scale_factor= 1/ratio, mode='nearest-exact')
-        bands_low_lr = F.interpolate(bands_low_lp, scale_factor= 1/ratio, mode='nearest-exact')
-
-    return bands_high_lr, bands_intermediate_lr, bands_low_lr
+    return bands_10_lr, bands_20_lr, bands_60_lr
