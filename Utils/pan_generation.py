58fe9e7b6b5b004e2c633099a370700f14df6f62
matciotola <matciotola@gmail.com>
2024-08-26 10:17:47 +0200

    change nomenclature

--- Utils/pan_generation.py
+++ Utils/pan_generation.py
@@ -2,23 +2,23 @@ import torch
 from Utils.spectral_tools import gen_mtf, mtf_kernel_to_torch
 from torch.nn.functional import conv2d, pad
 
-def selection(bands_high, bands_low, ratio, sat_name='S2-10'):
+def selection(pan, ms, ratio, sat_name='S2-10'):
 
-    h = mtf_kernel_to_torch(gen_mtf(ratio, sat_name)).to(bands_high.device)
+    h = mtf_kernel_to_torch(gen_mtf(ratio, sat_name)).type(pan.dtype).to(pan.device)
 
     # Decimation
-    bands_high_lpf = conv2d(pad(bands_high, (h.shape[-1] // 2, h.shape[-1] // 2, h.shape[-1] // 2, h.shape[-1] // 2), mode='replicate'), h, groups=bands_high.shape[1])
-    bands_high_lr = bands_high_lpf[:, :, 1::ratio, 1::ratio]
+    pan_lpf = conv2d(pad(pan, (h.shape[-1] // 2, h.shape[-1] // 2, h.shape[-1] // 2, h.shape[-1] // 2), mode='replicate'), h, groups=pan.shape[1])
+    pan_lr = pan_lpf[:, :, 1::ratio, 1::ratio]
 
     # CorrCoeff Calculation
 
     corr_coeff_global = []
 
-    for i in range(bands_high_lr.shape[1]):
-        selected_high = bands_high_lr[:, i, :, :].flatten()[None, :]
+    for i in range(pan_lr.shape[1]):
+        selected_high = pan_lr[:, i, :, :].flatten()[None, :]
         corr_coeff_selected_high = []
-        for j in range(bands_low.shape[1]):
-            selected_low = bands_low[:, j, :, :].flatten()[None, :]
+        for j in range(ms.shape[1]):
+            selected_low = ms[:, j, :, :].flatten()[None, :]
             X = torch.cat([selected_high, selected_low], dim=0)
             corr_coeff_selected_high.append(torch.corrcoef(X)[0,1])
         corr_coeff_global.append(torch.Tensor(corr_coeff_selected_high))
@@ -26,7 +26,7 @@ def selection(bands_high, bands_low, ratio, sat_name='S2-10'):
     corr_coeff_global = torch.vstack(corr_coeff_global)
 
     _, selected_bands = torch.max(corr_coeff_global, dim=0)
-    high_composed = bands_high[:, selected_bands, :, :]
+    high_composed = pan[:, selected_bands, :, :]
 
     return high_composed, selected_bands
 
@@ -38,41 +38,41 @@ def vectorized_alpha_estimation (a, b):
     alpha = alpha.solution[:, :, :, None]
 
     return alpha
-def synthesize(bands_high, bands_low,  ratio, sat_name='S2-10'):
+def synthesize(pan, ms,  ratio, sat_name='S2-10'):
 
     scale = 1 / ratio
 
-    h = mtf_kernel_to_torch(gen_mtf(ratio, sat_name)).type(bands_high.dtype).to(bands_high.device)
-    bands_high_lpf = conv2d(
-                            pad(bands_high, (h.shape[-1] // 2,
+    h = mtf_kernel_to_torch(gen_mtf(ratio, sat_name)).type(pan.dtype).to(pan.device)
+    pan_lpf = conv2d(
+                            pad(pan, (h.shape[-1] // 2,
                                              h.shape[-1] // 2,
                                              h.shape[-1] // 2,
                                              h.shape[-1] // 2),
                                 mode='replicate'),
                             h,
-                            groups=bands_high.shape[1])
-    bands_high_lr = bands_high_lpf[:, :, 1::ratio, 1::ratio]
+                            groups=pan.shape[1])
+    pan_lr = pan_lpf[:, :, 1::ratio, 1::ratio]
 
-    padding_high_lr = torch.ones((bands_high_lr.shape[0], 1, bands_high_lr.shape[2], bands_high_lr.shape[3]), dtype=bands_high_lr.dtype,
-                              device=bands_high_lr.device)
-    bands_high_lr_plus = torch.cat([padding_high_lr, bands_high_lr], dim=1)
+    padding_high_lr = torch.ones((pan_lr.shape[0], 1, pan_lr.shape[2], pan_lr.shape[3]), dtype=pan_lr.dtype,
+                              device=pan_lr.device)
+    pan_lr_plus = torch.cat([padding_high_lr, pan_lr], dim=1)
 
-    alpha = vectorized_alpha_estimation(bands_high_lr_plus.double(), bands_low.double()).float()
+    alpha = vectorized_alpha_estimation(pan_lr_plus.double(), ms.double()).float()
 
-    bands_high_exp = torch.ones((bands_high.shape[0], bands_high.shape[1] + 1, bands_high.shape[2], bands_high.shape[3]),
-                                 dtype=bands_high.dtype,
-                                 device=bands_high.device)
+    pan_exp = torch.ones((pan.shape[0], pan.shape[1] + 1, pan.shape[2], pan.shape[3]),
+                                 dtype=pan.dtype,
+                                 device=pan.device)
 
-    bands_high_exp[:, 1:, :, :] = bands_high
+    pan_exp[:, 1:, :, :] = pan
 
     synthesized_bands = []
     for i in range(alpha.shape[2]):
         a = alpha[:, :, i, :, None]
-        synthesized_bands.append(torch.sum(bands_high_exp * a, dim=1, keepdim=True))
+        synthesized_bands.append(torch.sum(pan_exp * a, dim=1, keepdim=True))
 
     synthesized_bands = torch.cat(synthesized_bands, dim=1)
 
-    selected_bands = list(range(bands_low.shape[1]))
+    selected_bands = list(range(ms.shape[1]))
 
     return synthesized_bands, selected_bands
 
