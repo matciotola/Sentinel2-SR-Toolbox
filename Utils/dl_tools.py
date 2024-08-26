1441f26cbaacad4b12c231767a797b0e2f2b73f7
matciotola <matciotola@gmail.com>
2024-08-26 10:07:45 +0200

    change datasets implementation

--- Utils/dl_tools.py
+++ Utils/dl_tools.py
@@ -50,79 +50,75 @@ class TrainingDataset20m(Dataset):
         bands_low_downsampled = norm(bands_low_downsampled)
         bands_low_lr = norm(bands_low_lr)
 
-        self.patches_high_lr = get_patches(bands_high_downsampled, patch_size_hr)
-        self.patches_low_lr = get_patches(bands_low_downsampled, patches_size_lr)
-        self.patches_low = get_patches(bands_low_lr, patch_size_hr)
+class TrainingDataset60mRR(Dataset):
+    def __init__(self, paths, norm):
+        super(TrainingDataset60mRR, self).__init__()
+        images_10_d60 = []
+        images_20_d120 = []
+        images_60_d360 = []
+        images_60 = []
+
+        for i in range(len(paths)):
+            bands_10_d60, bands_20_d120, bands_60_d360, bands_60 = open_mat(paths[i])
+            images_10_d60.append(bands_10_d60)
+            images_20_d120.append(bands_20_d120)
+            images_60_d360.append(bands_60_d360)
+            images_60.append(bands_60)
+
+        images_10_d60 = torch.cat(images_10_d60, 0)
+        images_20_d120 = torch.cat(images_20_d120, 0)
+        images_60_d360 = torch.cat(images_60_d360, 0)
+        images_60 = torch.cat(images_60, 0)
+
+        images_10_d60 = norm(images_10_d60)
+        images_20_d120 = norm(images_20_d120)
+        images_60_d360 = norm(images_60_d360)
+        images_60 = norm(images_60)
+
+        self.patches_10_d60 = images_10_d60
+        self.patches_20_d120 = images_20_d120
+        self.patches_60_d360 = images_60_d360
+        self.patches_60 = images_60
 
     def __len__(self):
-        return self.patches_high_lr.shape[0]
+        return self.patches_60.shape[0]
 
     def __getitem__(self, index):
-        return self.patches_high_lr[index], self.patches_low_lr[index], self.patches_low[index]
+        return self.patches_10_d60[index], self.patches_20_d120[index], self.patches_60_d360, self.patches_60[index]
 
 
-class TrainingDataset60m(Dataset):
-    def __init__(self, bands_high_paths, bands_intermediate_lr_paths, bands_low_lr_paths, norm, input_prepro,
-                 get_patches, ratio=2, patches_size=33):
-        super(TrainingDataset60m, self).__init__()
+class TrainingDataset20mFR(Dataset):
+    def __init__(self, paths, norm):
+        super(TrainingDataset20mFR, self).__init__()
 
-        bands_low_lr = []
-        bands_intermediate_lr = []
-        bands_high = []
+        images_20 = []
+        images_10 = []
 
         for i in range(len(bands_high_paths)):
             bands_high.append(open_tiff(bands_high_paths[i]))
             bands_intermediate_lr.append(open_tiff(bands_intermediate_lr_paths[i]))
             bands_low_lr.append(open_tiff(bands_low_lr_paths[i]))
 
-        bands_high = torch.cat(bands_high, 0)
-        bands_intermediate_lr = torch.cat(bands_intermediate_lr, 0)
-        bands_low_lr = torch.cat(bands_low_lr, 0)
+        for i in range(len(paths)):
+            bands_10, bands_20, _, _ = open_mat(paths[i])
+            images_10.append(bands_10)
+            images_20.append(bands_20)
 
-        bands_high_downsampled, bands_intermediate_downsampled, bands_low_downsampled, bands_low_lr = input_prepro(
-            bands_high, bands_intermediate_lr, bands_low_lr, ratio)
+        images_10 = torch.cat(images_10, 0)
+        images_20 = torch.cat(images_20, 0)
 
         bands_high_downsampled = norm(bands_high_downsampled)
         bands_intermediate_downsampled = norm(bands_intermediate_downsampled)
         bands_low_downsampled = norm(bands_low_downsampled)
         bands_low_lr = norm(bands_low_lr)
+        images_10 = norm(images_10)
+        images_20 = norm(images_20)
 
-        self.patches_high_lr = get_patches(bands_high_downsampled, patches_size)
-        self.patches_intermediate_lr = get_patches(bands_intermediate_downsampled, patches_size)
-        self.patches_low_lr = get_patches(bands_low_downsampled, patches_size)
-        self.patches_low = get_patches(bands_low_lr, patches_size)
+        self.patches_10 = images_10
+        self.patches_20 = images_20
 
     def __len__(self):
-        return self.patches_high_lr.shape[0]
+        return self.patches_20.shape[0]
 
     def __getitem__(self, index):
-        return self.patches_high_lr[index], self.patches_intermediate_lr, self.patches_low_lr[index], self.patches_low[
-            index]
-
-"""
-def get_patches(bands, patches_size=33):
-    print('list_bands for patches: ' + str(len(bands)))
-    patches = []
-
-    _, b, c, r = bands.shape
-    cont = 0
-    for i in range(0, r - patches_size, patches_size):
-        for j in range(0, c - patches_size, patches_size):
-            p = bands[:, :, i:i + patches_size, j:j + patches_size]
-
-            patches.append(p)
-
-            cont += 1
-    patches = torch.cat(patches, dim=0)
-    return patches
-"""
-
-def get_patches(bands, patch_size=33):
-
-    patches = []
-    for i in range(bands.shape[2] // patch_size):
-        for j in range(bands.shape[3] // patch_size):
-            patches.append(bands[:, :, patch_size * i:patch_size * (i + 1), patch_size * j:patch_size * (j + 1)])
-
-    patches = torch.cat(patches, dim=0)
-    return patches
+        return self.patches_10[index], self.patches_20[index]
